e0c5e01e113e1d25fd808ad01936bc10307e2a47
Patryk Zieliński <patryk.zielinski93@gmail.com>
2019-02-05 22:02:05 +0100

    Add -phtread to CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,103 +1,77 @@
 #include <array>
-#include <mutex>
-#include <thread>
 #include <atomic>
-#include <iostream>
-#include <random>
+#include <zconf.h>
+#include <semaphore.h>
 
-using namespace std;
-
-mutex mtx;
 const int philosophers_number = 5;
 
-struct fork {
-    mutex mutex;
-};
+sem_t chopsticks[philosophers_number];
 
-struct table {
-    atomic<bool> ready{false};
-    array<fork, philosophers_number> forks;
-};
+int leftChopstick(int i) {
+    return i;
+}
 
-struct philosopher {
-private:
-    string const name;
-    table const &dinnertable;
-    fork &left_fork;
-    fork &right_fork;
-    thread lifethread;
-    mt19937 rng{random_device{}()};
-public:
-    philosopher(string_view n, table const &t, fork &l, fork &r) :
-            name(n), dinnertable(t), left_fork(l), right_fork(r), lifethread(&philosopher::dine, this) {
+int rightChopstick(int i) {
+    return (i + 1) % 5;
+}
+
+void *philosopherJob(void *arg) {
+    bool eat = true;
+    int id = (int) (uintptr_t) arg;
+    while (eat) {
+        usleep(400000);
+        if (leftChopstick(id) < rightChopstick(id)) {
+            printf("  Filozof %i, podnosi lewą pałeczkę\n", id);
+            sem_wait(&chopsticks[leftChopstick(id)]);
+            printf("  Filozof %i, podnosi prawą pałeczkę\n", id);
+            sem_wait(&chopsticks[rightChopstick(id)]);
+        } else {
+            printf("  Filozof %i, podnosi prawą pałeczkę\n", id);
+            sem_wait(&chopsticks[rightChopstick(id)]);
+            printf("  Filozof %i, podnosi lewą pałeczkę\n", id);
+            sem_wait(&chopsticks[leftChopstick(id)]);
+        }
+        printf("  Filozof %i, filozof podniósł obie pałeczki!\n", id);
+
+        // Eat
+        printf("Filozof %i, zaczyna jedzenie!\n", id);
+        eat = false;
+        usleep(400000);
+
+        // Release chopsticks following the resource hierarchy
+        if (leftChopstick(id) < rightChopstick(id)) {
+            printf("  Filozof %i, odkłada prawą pałeczkę\n", id);
+            sem_post(&chopsticks[rightChopstick(id)]);
+            printf("  Filozof %i, odkłada lewą pałeczkę\n", id);
+            sem_post(&chopsticks[leftChopstick(id)]);
+        } else {
+            printf("  Filozof %i, odkłada lewą pałeczkę\n", id);
+            sem_post(&chopsticks[leftChopstick(id)]);
+            printf("  Filozof %i, odkłada prawą pałeczkę\n", id);
+            sem_post(&chopsticks[rightChopstick(id)]);
+        }
+        printf("~~~~~~~~  Filozof %i, odłożył obie pałeczki i zakończył jedzenie!!1\n", id);
     }
-
-    ~philosopher() {
-        lifethread.join();
-    }
-
-    void dine() {
-        while (!dinnertable.ready);
-
-        do {
-            think();
-            eat();
-        } while (dinnertable.ready);
-    }
-
-    void print(string_view text) {
-        lock_guard<mutex> cout_lock(mtx);
-        cout << name << text << endl;
-    }
-
-    void eat() {
-        lock(left_fork.mutex, right_fork.mutex);
-
-        lock_guard<mutex> left_lock(left_fork.mutex, adopt_lock);
-        lock_guard<mutex> right_lock(right_fork.mutex, adopt_lock);
-
-        print(" started eating.");
-
-        static thread_local uniform_int_distribution<> dist(1, 6);
-        this_thread::sleep_for(chrono::milliseconds(dist(rng) * 50));
-
-        print(" finished eating.");
-    }
-
-    void think() {
-        static thread_local uniform_int_distribution<> wait(1, 6);
-        this_thread::sleep_for(chrono::milliseconds(wait(rng) * 150));
-
-        print(" is thinking.");
-    }
-};
-
-void dine() {
-    cout << "Dinner started!" << endl;
-
-    {
-        table table;
-        array<philosopher, philosophers_number> philosophers
-                {
-                        {
-                                {"P1", table, table.forks[0], table.forks[1]},
-                                {"P2", table, table.forks[1], table.forks[2]},
-                                {"P3", table, table.forks[2], table.forks[3]},
-                                {"P4", table, table.forks[3], table.forks[4]},
-                                {"P5", table, table.forks[4], table.forks[0]},
-                        }
-                };
-
-        table.ready = true;
-        this_thread::sleep_for(chrono::seconds(5));
-        table.ready = false;
-    }
-
-    cout << "Dinner done!" << endl;
+    return nullptr;
 }
 
 int main() {
-    dine();
+    auto *philosophersThreadsIds = static_cast<pthread_t *>(malloc(philosophers_number * sizeof(pthread_t)));
+
+    for (int i = 0; i < philosophers_number; ++i) {
+        sem_init(&chopsticks[i], 0, 1);
+        pthread_create(&philosophersThreadsIds[i], nullptr, philosopherJob, (void *) (uintptr_t) i);
+    }
+
+    // Wait for philosophers to finish
+    for (int i = 0; i < philosophers_number; ++i) {
+        pthread_join(philosophersThreadsIds[i], nullptr);
+    }
+
+    free(philosophersThreadsIds);
+    for (int i = 0; i < philosophers_number; ++i) {
+        sem_destroy(&chopsticks[i]);
+    }
 
     return 0;
 }
